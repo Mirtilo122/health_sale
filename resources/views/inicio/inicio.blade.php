e5c63c18307f8b2e3fd32aaeb9d483bfa83fa5dc
Mirtilo122 <160802640+Mirtilo122@users.noreply.github.com>
2025-02-27 17:40:49 -0300

    Update inicio.blade.php

--- resources/views/inicio/inicio.blade.php
+++ resources/views/inicio/inicio.blade.php
@@ -7,9 +7,9 @@
 @section('conteudo')
 
 <div class="container_inicial">
-    <h1>Formulário de Orçamentos</h1>
+    <h4>Formulário de Orçamentos</h4>
 
-    <h3>Quem solicita o orçamento?</h3>
+    <h5 class="mt-5">Quem solicita o orçamento?</h5>
 
     <button class="btn" onclick="window.location.href='medico';">
         <i class="fas fa-user-md"></i> Médico ou Cirurgião
